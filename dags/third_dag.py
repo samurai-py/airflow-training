0889fb420726edcac95157c53978329103df4e35
samurai-py <marioestevao@gmail.com>
2023-11-13 16:46:37 -0300

    Novas dags

--- dags/third_dag.py
+++ dags/third_dag.py
@@ -2,10 +2,10 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
-dag = DAG('second_dag', description='Segunda DAG', schedule_interval=None, start_date=datetime(2023, 3, 5), catchup=False)
+dag = DAG('third_dag', description='Terceira DAG', schedule_interval=None, start_date=datetime(2023, 3, 5), catchup=False)
 
 task1 = BashOperator(task_id='tsk1', bash_command="sleep 5", dag=dag)
 task2 = BashOperator(task_id='tsk2', bash_command="sleep 5", dag=dag)
 task3 = BashOperator(task_id='tsk3', bash_command="sleep 5", dag=dag)
 
-task1 >> [task2, task3]
+[task1, task2] >> task3
